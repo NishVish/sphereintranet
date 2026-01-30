3222b7fe33bc3ebba83d7d49c33b2452041b3332
Nishant Vishwakarma <42230092+NishVish@users.noreply.github.com>
2026-01-30 10:40:13 +0530

    Auth Update

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -7,60 +7,10 @@ use CodeIgniter\Controller;
 
 class Auth extends Controller
 {
-    // public function login()
-    // {
-    //     return view('auth/login');
-    // }
     public function login()
     {
-        $session = session();
-    $model = new UserModel();
-
-    // $email = trim($this->request->getPost('email'));
-    $password = //trim($this->request->getPost('password'));
-    $password = "Admin123";
-
-    $user = $model->where('password', $password)->first();
-
-    if (!$user) {
-        return redirect()->back()->with('error', 'No user found with this email.');
+        return view('auth/login');
     }
-
-if ($password === $user['password']) {
-$session->set([
-    'user_id'              => $user['id'],
-    'email'                => $user['email'],
-    'username'             => $user['email'],  // Or $user['name'] if preferred
-    'user_type'            => $user['category'],
-    'employee_id'          => $user['employee_id'],
-    'name'                 => $user['name'],
-    'designation'          => $user['designation'],
-    'phone'                => $user['phone'],
-    'address'              => $user['address'],
-    'department'           => $user['department'] ?? '',
-    'doj'                  => $user['doj'] ?? '',
-    'uan_no'               => $user['uan_no'] ?? '',
-    'fathers_name'         => $user['fathers_name'] ?? '',
-    'aadhaar_card'         => $user['aadhaar_card'] ?? '',
-    'pan_card'             => $user['pan_card'] ?? '',
-    'bank_account_number'  => $user['bank_account_number'] ?? '',
-    'ifsc_code'            => $user['ifsc_code'] ?? '',
-    'isLoggedIn'           => true,
-]);
-
-
-
-        return redirect()->to('/home');
-    } else {
-        // echo '<pre>';
-        // print_r($user);
-        // echo '</pre>';
-        // exit;
-
-        return redirect()->back()->with('error', 'Invalid credentials.');
-    }
-}
-
 
 public function attemptLogin()
 {
@@ -68,8 +18,8 @@ public function attemptLogin()
     $model = new UserModel();
 
     // $email = trim($this->request->getPost('email'));
-    $password = //trim($this->request->getPost('password'));
-    $password = "Admin123";
+   // $password = //trim($this->request->getPost('password'));
+    $password = "admin123";
 
     $user = $model->where('password', $password)->first();
 
@@ -78,6 +28,8 @@ public function attemptLogin()
     }
 
 if ($password === $user['password']) {
+// if (True) {
+
 $session->set([
     'user_id'              => $user['id'],
     'email'                => $user['email'],
@@ -100,6 +52,26 @@ $session->set([
 ]);
 
 
+// $session->set([
+//     'user_id'              => '1',
+//     'email'                => "admin@example.com",
+//     'username'             => 'Admin123',  // Or $user['name'] if preferred
+//     'user_type'            => 'Admin',
+//     'employee_id'          => 111,
+//     'name'                 => 'Admin',
+//     'designation'          => 'Admin',
+//     'phone'                => 98798798798,
+//     'address'              => 'Katras',
+//     'department'           => $user['department'] ?? '',
+//     'doj'                  => $user['doj'] ?? '',
+//     'uan_no'               => $user['uan_no'] ?? '',
+//     'fathers_name'         => $user['fathers_name'] ?? '',
+//     'aadhaar_card'         => $user['aadhaar_card'] ?? '',
+//     'pan_card'             => $user['pan_card'] ?? '',
+//     'bank_account_number'  => $user['bank_account_number'] ?? '',
+//     'ifsc_code'            => $user['ifsc_code'] ?? '',
+//     'isLoggedIn'           => true,
+// ]);
 
         return redirect()->to('/home');
     } else {
